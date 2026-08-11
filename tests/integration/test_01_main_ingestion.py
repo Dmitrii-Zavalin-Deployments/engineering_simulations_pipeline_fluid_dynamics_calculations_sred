9d14dc9c25052b532ff4bd08697e8b5b3785f0b2
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-08-11 09:37:23 -0600

    Phase 2: Core Sprint

--- tests/integration/test_01_main_ingestion.py
+++ tests/integration/test_01_main_ingestion.py
@@ -34,6 +34,6 @@ def test_main_cli_ingestion_stage(workspace_folder, monkeypatch):
                 assert e.code == 0
 
         assert spy_ingest.called
-        input_data, config_data = spy_ingest.spy_return
+        input_data, config_data = spy_ingest.call_args.result
         assert input_data["grid"]["nx"] == 4
         assert config_data["max_poisson_iterations"] == 2000
