de952b2f326777221acb4899654b9a16498235d9
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-08-11 08:31:32 -0600

    Phase 2: Core Sprint

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,7 @@ from src.archivist import archive_simulation_results
 from src.cpp_gate import step_simulation
 from src.generate_previews import generate_snapshot_preview
 from src.ingestion import load_and_validate_inputs
+from src.main import main
 from src.state import SolverState
 
 __all__ = [
@@ -15,5 +16,6 @@ __all__ = [
     "archive_simulation_results",
     "generate_snapshot_preview",
     "load_and_validate_inputs",
+    "main",
     "step_simulation",
 ]
