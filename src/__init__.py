25041b6415fe27aed9401b9881decdffa8e6e02a
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-08-15 09:22:04 -0600

    Phase 2: Core Sprint

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,6 @@ and C++ execution bridge interfaces.
 
 from src.archivist import archive_simulation_results
 from src.cpp_gate import step_simulation
-from src.generate_previews import generate_snapshot_preview
 from src.ingestion import load_and_validate_inputs
 from src.main import main
 from src.state import SolverState
@@ -14,7 +13,6 @@ from src.state import SolverState
 __all__ = [
     "SolverState",
     "archive_simulation_results",
-    "generate_snapshot_preview",
     "load_and_validate_inputs",
     "main",
     "step_simulation",
