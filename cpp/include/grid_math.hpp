6481ca9375a7e23ea5b8386c7ae607c9b5585590
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2026-08-22 10:09:54 -0600

    Phase 2: Core Sprint

--- cpp/include/grid_math.hpp
+++ cpp/include/grid_math.hpp
@@ -22,12 +22,6 @@ namespace navier_stokes_solver {
  * @brief Structure representing the grid dimensions and spacing.
  */
 struct GridDimensions {
-    double x_min;
-    double x_max;
-    double y_min;
-    double y_max;
-    double z_min;
-    double z_max;
     int nx;
     int ny;
     int nz;
